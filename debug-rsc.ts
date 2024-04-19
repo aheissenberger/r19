2a678b33a3f1cc9defa510863b9f5098312cfcaa
Andreas Heissenberger <andreas@heissenberger.at>
2024-04-19 23:25:52 +0200

    update

--- debug-rsc.ts
+++ debug-rsc.ts
@@ -25,32 +25,33 @@ Bun.serve({
     port
 });
 
-async function rscPOST(req) {
+  
+  export async function rscPOST(req:Request) {
     const actionReference = String(req.headers.get("rsa-reference"))
     const actionOrigin = String(req.headers.get("rsa-origin"))
     const url = new URL(req.url)
     // Resolve the action
     const [filepath, name] = actionReference.split("#")
     const action = (await import(`.${resolve(filepath)}`))[name]
-
+  
     let args // Decode the arguments
-    if (req.headers.get('content-type').startsWith("multipart/form-data")) {
-        const rs = webToNodeStream(req)
-        // Use busboy to streamingly parse the reply from form-data.
-        const bb = busboy({ headers: Object.fromEntries(req.headers.entries()) })
-        const reply = decodeReplyFromBusboy(bb, resolve("build/") + "/")
-        rs.pipe(bb)
-        args = await reply
+    if (req?.body && req.headers.get('content-type')?.startsWith("multipart/form-data")) {
+      const rs = webToNodeStream( req.body)
+      //@ts-ignore
+      const bb = busboy({ headers: Object.fromEntries(req.headers.entries()) })
+      const reply = decodeReplyFromBusboy(bb, resolve("build/") + "/")
+      rs.pipe(bb)
+      args = await reply
     } else {
-        args = await decodeReply(await req.text(), moduleBaseURL)
+      args = await decodeReply(await req.text(), moduleBaseURL)
     }
-
+  
     const returnValue = await action.apply(null, args) // Call the action
-
+  
     const props = Object.fromEntries(url.searchParams.entries()) // We will use the query as props for the page
     const root = (await import(resolve("build/app", `.${actionOrigin}/page.js`))).default(props)
     return renderToPipeableStream({ returnValue, root }, moduleBaseURL) // Render the app with the RSC, action result and the new root
-}
+  }
 
 
 function nodeToWebStream(nodeStream: { pipe: Function, on: Function }): ReadableStream {
